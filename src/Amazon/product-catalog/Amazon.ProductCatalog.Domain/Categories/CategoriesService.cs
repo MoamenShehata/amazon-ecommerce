eb36ccd3f4ea7731a54f8ea97073ee882866823a
Moamen Shehata <mshehata@sure.com.sa>
2025-12-20 15:33:32 +0200

    commit adding product/catgeory

--- src/Amazon/product-catalog/Amazon.ProductCatalog.Domain/Categories/CategoriesService.cs
+++ src/Amazon/product-catalog/Amazon.ProductCatalog.Domain/Categories/CategoriesService.cs
@@ -1,11 +1,13 @@
 ﻿using Amazon.ProductCatalog.Domain.Products;
 using Amazon.ProductCatalog.Domain.Products.ValueObjects;
+using EMP.SharedKernel;
 using EMP.SharedKernel.Repositories;
 
 namespace Amazon.ProductCatalog.Domain.Categories;
 
 public class CategoriesService(
-IRepository<Category, Guid> _categoriesRepository
+IRepository<Category, Guid> _categoriesRepository,
+IUnitOfWork _unitOfWork
 )
 {
     public async Task<Category> CreateAsync(string name, ProductPrice price)
@@ -13,6 +15,10 @@ IRepository<Category, Guid> _categoriesRepository
         var categoryBySameName = await _categoriesRepository.FilterSingleAsync(p => p.Name.ToLower() == name.ToLower());
         if (categoryBySameName != null) throw new Exception();
 
-        return new(name);
+        var category = new Category(name);
+        _categoriesRepository.Add(category);
+
+        await _unitOfWork.CommitAsync();
+        return category;
     }
 }
--- src/Amazon/product-catalog/Amazon.ProductCatalog.Domain/Products/ProductsService.cs
+++ src/Amazon/product-catalog/Amazon.ProductCatalog.Domain/Products/ProductsService.cs
@@ -1,12 +1,14 @@
 ﻿using Amazon.ProductCatalog.Domain.Categories;
 using Amazon.ProductCatalog.Domain.Products.ValueObjects;
+using EMP.SharedKernel;
 using EMP.SharedKernel.Repositories;
 
 namespace Amazon.ProductCatalog.Domain.Products;
 
 public class ProductsService(
     IRepository<Category, Guid> _categoriesRepository,
-    IRepository<Product, Guid> _productsRepository
+    IRepository<Product, Guid> _productsRepository,
+    IUnitOfWork _unitOfWork
     )
 {
     public async Task<Product> CreateAsync(Guid categoryId, string name, ProductPrice price)
@@ -16,6 +18,10 @@ public class ProductsService(
         var productBySameName = await _productsRepository.FilterSingleAsync(p => p.Name.ToLower() == name.ToLower());
         if (productBySameName != null) throw new Exception();
 
-        return category.NewProduct(name, price);
+        var product = category.NewProduct(name, price);
+        _productsRepository.Add(product);
+
+        await _unitOfWork.CommitAsync();
+        return product;
     }
 }
