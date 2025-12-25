1f8672a38bfd36655e9d103280fce1f0c61e32ef
Moamen Shehata <mshehata@sure.com.sa>
2025-12-25 18:52:49 +0200

    uses static services

--- src/Amazon/product-catalog/tests/Amazon.ProductCatalog.Domain.Tests/Categories/CategoriesServiceShould.cs
+++ src/Amazon/product-catalog/tests/Amazon.ProductCatalog.Domain.Tests/Categories/CategoriesServiceShould.cs
@@ -1,75 +1,26 @@
-﻿using System.Linq.Expressions;
-using Amazon.ProductCatalog.Domain.Categories;
-using EMP.SharedKernel.Repositories;
+﻿using Amazon.ProductCatalog.Domain.Categories;
 
 namespace Amazon.ProductCatalog.Domain.Tests.Categories
 {
-    public class TestRepo : IRepository<Category, Guid>
-    {
-        private List<Category> _categories = new()
-        {
-            new Category("Furniture",null),
-            new Category("Electronics",null),
-            new Category("Shoes",null),
-            new Category("Clothes", null),
-        };
-
-        public void Add(Category aggregate)
-        {
-            _categories.Add(aggregate);
-        }
-
-        public Task<int> CountAsync(Expression<Func<Category, bool>> predicate)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IEnumerable<Category>> FilterAsync(Expression<Func<Category, bool>> predicate)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Category> FilterSingleAsync(Expression<Func<Category, bool>> predicate)
-        {
-            return Task.FromResult(_categories.FirstOrDefault(predicate.Compile()));
-        }
-
-        public Task<Category> FilterSingleAsync<TProperty>(Expression<Func<Category, bool>> predicate, Func<IQueryable<Category>, Microsoft.EntityFrameworkCore.Query.IIncludableQueryable<Category, TProperty>> include)
-        {
-            return Task.FromResult(_categories.FirstOrDefault(predicate.Compile()));
-        }
-
-        public Task<IEnumerable<Category>> GetAllAsync()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Category> GetByIdAsync(Guid id)
-        {
-            return Task.FromResult(_categories.FirstOrDefault(x => x.Id == id));
-        }
-
-        public Task<Category> GetByIdAsync<TProperty>(Guid id, Expression<Func<Category, TProperty>> include)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Remove(Category aggregate)
-        {
-            throw new NotImplementedException();
-        }
-    }
 
     public class CategoriesServiceShould
     {
+        private static CategoriesService _service;
+        private static TestRepo _testRepo;
+
+        public CategoriesServiceShould()
+        {
+            _testRepo = new TestRepo();
+            _service = new CategoriesService(_testRepo);
+        }
+
         [Theory]
         [InlineData("Furniture")]
         [InlineData("Shoes")]
         [InlineData("Electronics")]
         public async Task Throw_When_CategoryName_Exists(string categoryName)
         {
-            var service = GetService();
-            await Assert.ThrowsAnyAsync<Exception>(async () => await service.CreateAsync(categoryName, null));
+            await Assert.ThrowsAnyAsync<Exception>(async () => await _service.CreateAsync(categoryName, null));
         }
 
         [Theory]
@@ -78,8 +29,7 @@ namespace Amazon.ProductCatalog.Domain.Tests.Categories
         [InlineData("Airpods")]
         public async Task Create_New_Categories_WithoutParents(string categoryName)
         {
-            var service = GetService();
-            var createdCategory = await service.CreateAsync(categoryName, null);
+            var createdCategory = await _service.CreateAsync(categoryName, null);
 
             Assert.Equal(categoryName, createdCategory.Name);
         }
@@ -90,12 +40,9 @@ namespace Amazon.ProductCatalog.Domain.Tests.Categories
         [InlineData("Airpods", "Electronics")]
         public async Task Create_New_Categories_WithParents(string categoryName, string parentCategoryName)
         {
-            var repoMoq = new TestRepo();
-            var service = new CategoriesService(repoMoq);
+            var parentCategory = await _testRepo.FilterSingleAsync(c => c.Name == parentCategoryName);
 
-            var parentCategory = await repoMoq.FilterSingleAsync(c => c.Name == parentCategoryName);
-
-            var createdCategory = await service.CreateAsync(categoryName, parentCategory.Id);
+            var createdCategory = await _service.CreateAsync(categoryName, parentCategory.Id);
 
             Assert.Equal(categoryName, createdCategory.Name);
             Assert.Equal(parentCategoryName, parentCategory.Name);
@@ -104,16 +51,7 @@ namespace Amazon.ProductCatalog.Domain.Tests.Categories
         [Fact]
         public async Task Throw_To_Update_Non_Existing()
         {
-            var service = GetService();
-
-            await Assert.ThrowsAnyAsync<Exception>(async () => await service.UpdateAsync(Guid.NewGuid(), "newCategoryName", null));
-        }
-
-        private static CategoriesService GetService()
-        {
-            var repoMoq = new TestRepo();
-            var service = new CategoriesService(repoMoq);
-            return service;
+            await Assert.ThrowsAnyAsync<Exception>(async () => await _service.UpdateAsync(Guid.NewGuid(), "newCategoryName", null));
         }
 
         //to do we need to get a category in the service
@@ -123,12 +61,9 @@ namespace Amazon.ProductCatalog.Domain.Tests.Categories
         [InlineData("Electronics")]
         public async Task Throw_To_Update_Using_Existing_Category_Names(string categoryName)
         {
-            var repoMoq = new TestRepo();
-            var service = new CategoriesService(repoMoq);
+            var categoryToUpdate = await _testRepo.FilterSingleAsync(c => c.Name == categoryName);
 
-            var categoryToUpdate = await repoMoq.FilterSingleAsync(c => c.Name == categoryName);
-
-            await Assert.ThrowsAnyAsync<Exception>(async () => await service.UpdateAsync(categoryToUpdate.Id, categoryName, null));
+            await Assert.ThrowsAnyAsync<Exception>(async () => await _service.UpdateAsync(categoryToUpdate.Id, categoryName, null));
         }
 
         [Theory]
@@ -137,12 +72,9 @@ namespace Amazon.ProductCatalog.Domain.Tests.Categories
         [InlineData("Electronics", "Electronics-")]
         public async Task Update(string categoryName, string newName)
         {
-            var repoMoq = new TestRepo();
-            var service = new CategoriesService(repoMoq);
+            var categoryToUpdate = await _testRepo.FilterSingleAsync(c => c.Name == categoryName);
 
-            var categoryToUpdate = await repoMoq.FilterSingleAsync(c => c.Name == categoryName);
-
-            await service.UpdateAsync(categoryToUpdate.Id, newName, null);
+            await _service.UpdateAsync(categoryToUpdate.Id, newName, null);
         }
     }
 }
---
+++ src/Amazon/product-catalog/tests/Amazon.ProductCatalog.Domain.Tests/Categories/TestRepo.cs
@@ -0,0 +1,62 @@
+﻿using System.Linq.Expressions;
+using Amazon.ProductCatalog.Domain.Categories;
+using EMP.SharedKernel.Repositories;
+
+namespace Amazon.ProductCatalog.Domain.Tests.Categories
+{
+    public class TestRepo : IRepository<Category, Guid>
+    {
+        private List<Category> _categories = new()
+        {
+            new Category("Furniture",null),
+            new Category("Electronics",null),
+            new Category("Shoes",null),
+            new Category("Clothes", null),
+        };
+
+        public void Add(Category aggregate)
+        {
+            _categories.Add(aggregate);
+        }
+
+        public Task<int> CountAsync(Expression<Func<Category, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Category>> FilterAsync(Expression<Func<Category, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Category> FilterSingleAsync(Expression<Func<Category, bool>> predicate)
+        {
+            return Task.FromResult(_categories.FirstOrDefault(predicate.Compile()));
+        }
+
+        public Task<Category> FilterSingleAsync<TProperty>(Expression<Func<Category, bool>> predicate, Func<IQueryable<Category>, Microsoft.EntityFrameworkCore.Query.IIncludableQueryable<Category, TProperty>> include)
+        {
+            return Task.FromResult(_categories.FirstOrDefault(predicate.Compile()));
+        }
+
+        public Task<IEnumerable<Category>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Category> GetByIdAsync(Guid id)
+        {
+            return Task.FromResult(_categories.FirstOrDefault(x => x.Id == id));
+        }
+
+        public Task<Category> GetByIdAsync<TProperty>(Guid id, Expression<Func<Category, TProperty>> include)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Category aggregate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
