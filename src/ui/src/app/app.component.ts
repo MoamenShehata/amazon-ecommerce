efe0793ea19cc440144a8465401398478abcfa7d
Moamen Shehata <engmoamen00@gmail.com>
2026-01-18 16:30:18 +0200

    app layout

--- src/ui/src/app/app.component.ts
+++ src/ui/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AuthService } from './authentication/services/authentication.service';
 import { AppServicesProvider } from './core/services/app-services.provider';
+import { IdentityControlsComponent } from './authentication/components/authentication-landing/identity-controls/identity-controls.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, IdentityControlsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
