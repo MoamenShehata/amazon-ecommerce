15f7ea1c61eaeec50653b460f89f0fd82ea61542
moamen <engmoamen00@gmail.com>
2026-06-01 11:45:08 +0300

    layout for my profile

---
+++ src/ui/src/app/customers/components/customer-profile-layout/customer-profile-layout.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from "@angular/router";
+
+@Component({
+  selector: 'customer-profile-layout',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './customer-profile-layout.component.html',
+  styleUrl: './customer-profile-layout.component.css'
+})
+export class CustomerProfileLayoutComponent {
+
+}
--- src/ui/src/app/customers/customer.routes.ts
+++ src/ui/src/app/customers/customer.routes.ts
@@ -3,18 +3,26 @@ import { authGuard } from "../core/guards/auth-guard";
 import { MyOrdersComponent } from "./components/my-orders/my-orders.component";
 import { OrderDetailsComponent } from "./components/order-details/order-details.component";
 import { CustomerProfileComponent } from "./components/customer-profile/customer-profile.component";
+import { CustomerProfileLayoutComponent } from "./components/customer-profile-layout/customer-profile-layout.component";
 
 export const CustomerRoutes: Routes = [
   {
-    path: "profile",
-    component: CustomerProfileComponent,
+    path: "",
+    component: CustomerProfileLayoutComponent,
+    children: [
+      {
+        path: "profile",
+        component: CustomerProfileComponent,
+      },
+      {
+        path: "orders",
+        component: MyOrdersComponent,
+      },
+      {
+        path: "orders/:id",
+        component: OrderDetailsComponent,
+      },
+    ]
   },
-  {
-    path: "orders",
-    component: MyOrdersComponent,
-  },
-  {
-    path: "orders/:id",
-    component: OrderDetailsComponent,
-  },
+
 ];
