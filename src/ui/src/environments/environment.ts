ad34992bad3efea6d58e118d77c0d2e9189ccf1b
moamen <engmoamen00@gmail.com>
2026-06-02 08:30:45 +0300

    dockerize auth service

--- src/ui/src/environments/environment.ts
+++ src/ui/src/environments/environment.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: false,
 
   authenticationBaseUrl: "https://localhost:5001",
+  // authenticationBaseUrl: "http://localhost:5000",
   catalogBaseUrl: "https://localhost:7182/api",
   cartBaseUrl: "https://localhost:7101/api",
   customersBaseUrl: "https://localhost:7128/api",
