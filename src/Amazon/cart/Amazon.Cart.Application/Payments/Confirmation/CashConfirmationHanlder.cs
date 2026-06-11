2f6488c07e730ebe6e1aae761f4787aaca9edcaa
moamen <engmoamen00@gmail.com>
2026-06-11 14:06:46 +0300

    organize confirmation handlers

---
+++ src/Amazon/cart/Amazon.Cart.Application/Payments/Confirmation/CashConfirmationHanlder.cs
@@ -0,0 +1,23 @@
+﻿using Amazon.Cart.Application.Dtos;
+using Amazon.SharedKernel.API;
+using Amazon.SharedKernel.Common.Services;
+using Amazon.SharedKernel.Orders.Events;
+
+namespace Amazon.Cart.Application.Payments.Confirmation;
+
+public class CashConfirmationHanlder(
+    IOtpService _otpService
+    ) : IPaymentMethodConfirmationHanlder
+{
+    public async Task<RestResponse<CheckoutPaymentInfo>> ConfirmAsync(ConfirmPaymentRequest request, Guid customerId, decimal amount)
+    {
+        if (string.IsNullOrWhiteSpace(request.Otp))
+            return RestResponse<CheckoutPaymentInfo>.BadRequest("Please provide a valid otp");
+
+        var isOtpValid = await _otpService.ValidateAsync(customerId, request.Otp);
+        if (!isOtpValid)
+            return RestResponse<CheckoutPaymentInfo>.BadRequest($"Invalid otp {request.Otp}");
+
+        return RestResponse<CheckoutPaymentInfo>.Success(new CashOnDeliveryCheckoutInfo());
+    }
+}
---
+++ src/Amazon/cart/Amazon.Cart.Application/Payments/Confirmation/VisaConfirmationHanlder.cs
@@ -0,0 +1,25 @@
+﻿using Amazon.Cart.Application.Dtos;
+using Amazon.Cart.Application.Payments.Validators;
+using Amazon.Cart.Domain.Payments;
+using Amazon.SharedKernel.API;
+using Amazon.SharedKernel.Orders.Events;
+
+namespace Amazon.Cart.Application.Payments.Confirmation;
+
+public class VisaConfirmationHanlder(
+        PaymentsService _paymentsService
+    ) : IPaymentMethodConfirmationHanlder
+{
+    public async Task<RestResponse<CheckoutPaymentInfo>> ConfirmAsync(ConfirmPaymentRequest request, Guid customerId, decimal amount)
+    {
+        var validationResult = new CheckoutUsingVisaRequestValidator().Validate(request.VisaDetails);
+        if (!validationResult.IsValid)
+            return RestResponse<CheckoutPaymentInfo>.BadRequest(validationResult.Errors.FirstOrDefault().ErrorMessage);
+
+        var chargeCardResult = await _paymentsService.ChargePaymentCardForAmountAsync(customerId, request.VisaDetails.PaymentCardId, request.VisaDetails.Cvv, amount);
+        if (!chargeCardResult.IsSuccess)
+            return RestResponse<CheckoutPaymentInfo>.BadRequest(chargeCardResult.Error.ToString());
+
+        return RestResponse<CheckoutPaymentInfo>.Success(new PaymentCardCheckoutInfo(request.VisaDetails.PaymentCardId, chargeCardResult.Value));
+    }
+}
