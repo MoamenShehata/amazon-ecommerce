2d2e519d68c81594733b66e24fe15d5a08bb79d2
moamen <engmoamen00@gmail.com>
2026-06-11 12:14:42 +0300

    fix cart items

--- src/Amazon/cart/Amazon.Cart.Domain/Specifications/ShoppingCartSpecification.cs
+++ src/Amazon/cart/Amazon.Cart.Domain/Specifications/ShoppingCartSpecification.cs
@@ -1,22 +1,21 @@
 ﻿using Amazon.Cart.Domain.Products;
 using Amazon.Cart.Domain.ShoppingCarts;
 using Amazon.SharedKernel.API;
-using Moamen.SDKs.Repository;
 
 namespace Amazon.Cart.Domain.Specifications;
 
 public class ShoppingCartSpecification(
-        IRepository<Product, Guid> _products
+        IProductsRepo _products
     )
 {
     public async Task<RestResponse<bool>> SatisfiesAsync(ShoppingCart cart)
     {
-        var products = await _products.GetAllAsync(p => cart.AggregatToProducts.Select(x => x.Key).Contains(p.Id));
+        var products = await _products.GetCartProductsAsync(cart);
 
-        foreach (var cartProduct in cart.AggregatToProducts)
+        foreach (var cartItem in cart.Items)
         {
-            var systemProduct = products.FirstOrDefault(x => x.Id == cartProduct.Key);
-            if (systemProduct.IsDeleted)
+            var systemProduct = products.FirstOrDefault(x => x.Id == cartItem.ProductId);
+            if (systemProduct is null || systemProduct.IsDeleted)
                 return RestResponse<bool>.BadRequest($"Shopping cart is in invalid statues, product named {systemProduct.Info.Name} is not in our system");
         }
 
