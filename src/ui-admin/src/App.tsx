bff1f0fdf679378ce27dd8fa49d086c6b6ec2f0f
moamen <engmoamen00@gmail.com>
2026-06-24 16:55:15 +0300

    layout

--- src/ui-admin/src/App.tsx
+++ src/ui-admin/src/App.tsx
@@ -1,15 +1,44 @@
-import './App.css'
-import LoadingSpinner from './core/components/loading-spinner/loading-spinner'
-import ProductList from './product-catalog/components/products-list'
+import "./App.css";
+import LoadingSpinner from "./core/components/loading-spinner/loading-spinner";
+import IdentityControls from "./identity-controls";
+import ProductList from "./product-catalog/components/products-list";
 
 function App() {
-
   return (
     <>
       <LoadingSpinner />
-      <ProductList />
+      <div className="min-vh-100 bg-light">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+          <div className="container">
+            <a className="navbar-brand fw-bold" href="#">
+              Amazon
+            </a>
+
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <IdentityControls isAuthenticated={false} />
+            </div>
+          </div>
+        </nav>
+
+        <div className="container my-5">
+          <ProductList />
+          {/* <router-outlet></router-outlet> */}
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/ui-admin/src/identity-controls.tsx
@@ -0,0 +1,41 @@
+export default function IdentityControls({
+  isAuthenticated,
+}: Readonly<{ isAuthenticated: boolean }>) {
+  function startOAuthLogin() {}
+  function logout() {}
+
+  return (
+    <div className="navbar-nav ms-auto">
+      {!isAuthenticated && (
+        <li className="nav-link">
+          <button
+            className="btn btn-outline-light ms-2"
+            type="button"
+            onClick={startOAuthLogin}
+          >
+            Login
+          </button>
+        </li>
+      )}
+
+      {isAuthenticated && (
+        <>
+          <li className="nav-link">
+            <span>Email</span>
+          </li>
+
+          <li className="nav-item">
+            {/* <a routerLink="/admin/orders" className="nav-link">All orders</a> */}
+            <a className="nav-link">All orders</a>
+          </li>
+
+          <li className="nav-link">
+            <button className="btn btn-outline-light ms-2" onClick={logout}>
+              Logout
+            </button>
+          </li>
+        </>
+      )}
+    </div>
+  );
+}
