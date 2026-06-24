0c0e89614858d4e49ad31e4cf157701d5375f85e
moamen <engmoamen00@gmail.com>
2026-06-24 16:43:46 +0300

    install prettier

--- src/ui-admin/src/product-catalog/components/products-list.tsx
+++ src/ui-admin/src/product-catalog/components/products-list.tsx
@@ -9,74 +9,87 @@ import catalogServices from "../services/catalog.services";
 import type { PageRequest } from "../../core/models/page-request.models";
 import DataPaginator from "../../core/components/data-paginator/data-paginator";
 
-export default function ProductList({ }) {
-    const pageSize = 1;
+export default function ProductList({}) {
+  const pageSize = 1;
 
-    const [productsPage, setProductsPage] = useState<PagedResult<ProductForListModel> | null>(null);
-    const [lastSeenValue, setLastSeenValue] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
-    const [pageNumber, setPageNumber] = useState(1);
+  const [productsPage, setProductsPage] =
+    useState<PagedResult<ProductForListModel> | null>(null);
+  const [lastSeenValue, setLastSeenValue] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [pageNumber, setPageNumber] = useState(1);
 
-    useEffect(() => {
-        let pageRequest: PageRequest = {
-            pageNumber: pageNumber,
-            pageSize: pageSize,
-            lastSeenValue: lastSeenValue,
-        };
+  useEffect(() => {
+    let pageRequest: PageRequest = {
+      pageNumber: pageNumber,
+      pageSize: pageSize,
+      lastSeenValue: lastSeenValue,
+    };
 
-        catalogServices.getProductsPage(pageRequest)
-            .then(page => {
-                setProductsPage(page.data);
-                setLastSeenValue(page.data.lastSeenValue);
-                setIsLoading(false);
-            }, err => {
-                alert('Error');
-                console.log(err)
-            })
-
-        return () => { console.log('clean up getting product page') }
-    }, [pageNumber]);
-
-
-    let productsHeader = <HeaderWithButton header='Products' displayButton={true} onClick={openForm} />;
-
-    function openForm() {
-        alert('new product form');
-    }
-
-    function onProductDeleted(product: ProductForListModel) {
-        setProductsPage({
-            totalCount: productsPage!?.totalCount - 1,
-            lastSeenValue: productsPage?.lastSeenValue,
-            items: productsPage?.items.filter(x => x.id != product.id)!
-        })
-    }
-
-    let productsDiv = (productsPage?.items || []).map(p => <ProductPreview product={p} onDeleted={() => onProductDeleted(p)} />);
-
-    return (
-        <Container classes="p-2">
-            {productsHeader}
-
-            {isLoading && (
-                <div className="alert alert-info" role="alert">
-                    Loading products...
-                </div>
-            )}
-
-            {(productsPage && !isLoading) && (
-                <>
-                    <div className="row mt-4" >
-                        <MayBeEmptyList list={productsPage.items} component={productsDiv} />
-                    </div >
-
-                    <DataPaginator currentPageNumber={pageNumber} totalCount={productsPage.totalCount} pageSize={pageSize} onPageChanged={setPageNumber} />
-                </>
-            )
-            }
-
-
-
-        </Container >
+    catalogServices.getProductsPage(pageRequest).then(
+      (page) => {
+        setProductsPage(page.data);
+        setLastSeenValue(page.data.lastSeenValue);
+        setIsLoading(false);
+      },
+      (err) => {
+        alert("Error");
+        console.log(err);
+      },
     );
+
+    return () => {
+      console.log("clean up getting product page");
+    };
+  }, [pageNumber]);
+
+  let productsHeader = (
+    <HeaderWithButton
+      header="Products"
+      displayButton={true}
+      onClick={openForm}
+    />
+  );
+
+  function openForm() {
+    alert("new product form");
+  }
+
+  function onProductDeleted(product: ProductForListModel) {
+    setProductsPage({
+      totalCount: productsPage!?.totalCount - 1,
+      lastSeenValue: productsPage?.lastSeenValue,
+      items: productsPage?.items.filter((x) => x.id != product.id)!,
+    });
+  }
+
+  let productsDiv = (productsPage?.items || []).map((p) => (
+    <ProductPreview product={p} onDeleted={() => onProductDeleted(p)} />
+  ));
+
+  return (
+    <Container classes="p-2">
+      {productsHeader}
+
+      {isLoading && (
+        <div className="alert alert-info" role="alert">
+          Loading products...
+        </div>
+      )}
+
+      {productsPage && !isLoading && (
+        <>
+          <div className="row mt-4">
+            <MayBeEmptyList list={productsPage.items} component={productsDiv} />
+          </div>
+
+          <DataPaginator
+            currentPageNumber={pageNumber}
+            totalCount={productsPage.totalCount}
+            pageSize={pageSize}
+            onPageChanged={setPageNumber}
+          />
+        </>
+      )}
+    </Container>
+  );
 }
