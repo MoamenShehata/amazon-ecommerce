b4ead651629c0cede79e5b89069d7865a3fa48f2
moamen <engmoamen00@gmail.com>
2026-06-25 17:28:21 +0300

    add routing

--- src/ui-admin/src/App.tsx
+++ src/ui-admin/src/App.tsx
@@ -1,3 +1,4 @@
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import "./App.css";
 import Container from "./core/bootstrap/components/bootstrap-container";
 import LoadingSpinner from "./core/components/loading-spinner/loading-spinner";
@@ -5,41 +6,54 @@ import IdentityControls from "./identity-controls";
 import ProductList from "./product-catalog/components/products-list";
 
 function App() {
-  return (
-    <>
-      <LoadingSpinner />
-      <div className="min-vh-100 bg-light">
-        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-          <Container>
-            <a className="navbar-brand fw-bold" href="#">
-              Amazon
-            </a>
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <>
+          <LoadingSpinner />
 
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarNav"
-              aria-controls="navbarNav"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
+          <div className="min-vh-100 bg-light">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+              <Container>
+                <a className="navbar-brand fw-bold" href="#">
+                  Amazon
+                </a>
 
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <IdentityControls isAuthenticated={false} />
-            </div>
-          </Container>
-        </nav>
+                <button
+                  className="navbar-toggler"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#navbarNav"
+                  aria-controls="navbarNav"
+                  aria-expanded="false"
+                  aria-label="Toggle navigation"
+                >
+                  <span className="navbar-toggler-icon"></span>
+                </button>
 
-        <Container classes="my-5">
-          <ProductList />
-          {/* <router-outlet></router-outlet> */}
-        </Container>
-      </div>
-    </>
-  );
+                <div className="collapse navbar-collapse" id="navbarNav">
+                  <IdentityControls isAuthenticated={false} />
+                </div>
+              </Container>
+            </nav>
+
+            <Container classes="my-5">
+              <Outlet />
+            </Container>
+          </div>
+        </>
+      ),
+      children: [
+        {
+          path: "products",
+          element: <ProductList />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
